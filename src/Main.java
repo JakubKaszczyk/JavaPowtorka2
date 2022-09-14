ad6a6aaa69a46de66461ddec467f6b61fdaa91ea
JakubKaszczyk <geraldzrivi4@gmail.com>
2022-09-14 15:45:20 +0200

    java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Sortowanie sortowanie1 = new Sortowanie();
+        sortowanie1.wypełnijZKlawiatury();
+        sortowanie1.wypiszTablice();
+
+        sortowanie1.sortuj();
+        sortowanie1.wypiszTablice();
+    }
+
+
+}
---
+++ src/Sortowanie.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Sortowanie {
+
+    public int[] tablica = new int[10];
+
+    public void wypełnijZKlawiatury() {
+        Scanner klawiaturaWczytane = new Scanner(System.in);
+        System.out.println("Wprowadź elementy tablicy");
+
+        for (int i = 0; i < tablica.length; i++) {
+            System.out.println("Element " + i + " :");
+            tablica[i] = klawiaturaWczytane.nextInt();
+        }
+
+
+    }
+
+    public void wypiszTablice() {
+        System.out.println("Tablica:");
+        for (int element : tablica) {
+            System.out.println(element + " ");
+        }
+    }
+
+    private int szukajMaksymalnej(int k) {
+
+        int szukanyIndeks = k;
+        int maksymalnaWartoc = tablica[k];
+        for (int i = k + 1; i < tablica.length; i++) {
+            if (maksymalnaWartoc < tablica[i]) {
+                maksymalnaWartoc = tablica[i];
+                szukanyIndeks = i;
+            }
+        }
+
+        return szukanyIndeks;
+    }
+
+    public void sortuj() {
+        for (int i = 0; i < tablica.length; i++) {
+            int indeks = szukajMaksymalnej(i);
+            int pomoc = tablica[i];
+            tablica[i] = tablica[indeks];
+            tablica[indeks] = pomoc;
+        }
+
+    }
+}
